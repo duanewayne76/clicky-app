f7d862b1a97eb134997eb5e6313de1e10333ddac
Duane Stafford <duanestafford@Duanes-MBP.richmond.edu>
2019-10-03 18:11:20 -0400

    Updates

---
+++ precache-manifest.ff35468511ab738e33606c83dec9f830.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b06bddd554ae836b9f7f0995fa6a50d",
+    "url": "/clicky-app/index.html"
+  },
+  {
+    "revision": "f1bbc605cbbfd997d036",
+    "url": "/clicky-app/static/css/main.dc2f4077.chunk.css"
+  },
+  {
+    "revision": "e8ee37d32af8b139cbdf",
+    "url": "/clicky-app/static/js/2.8266f532.chunk.js"
+  },
+  {
+    "revision": "f1bbc605cbbfd997d036",
+    "url": "/clicky-app/static/js/main.e5094e15.chunk.js"
+  },
+  {
+    "revision": "f2f254986aebb7a9bb44",
+    "url": "/clicky-app/static/js/runtime-main.79671430.js"
+  },
+  {
+    "revision": "67c81d8fd4c36e8e9cd9b3ddaab5c5cb",
+    "url": "/clicky-app/static/media/gijoe_black.67c81d8f.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e18eddee38757726ad9e49fc120fa7bc.js"
+  "/clicky-app/precache-manifest.ff35468511ab738e33606c83dec9f830.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/clicky-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
